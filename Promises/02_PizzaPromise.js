f9882bab6b4aad71ff849769ce6dcacef30f4063
Suraj-jha7 <123754719+Suraj-jha7@users.noreply.github.com>
2023-11-01 10:22:03 +0000

    pizza promise commited

---
+++ Promises/02_PizzaPromise.js
@@ -0,0 +1,52 @@
+function getCheese(){
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            const cheese="cheesy";
+            // console.log("here is the cheese",cheese);
+            resolve(cheese);
+        },2000)
+    })
+}
+
+
+function makeDough(cheese)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            const dough=cheese+"dough";
+            // console.log("here is the dough",dough);
+            resolve(dough);
+        }, 2000);
+    });
+}
+
+
+function bakePizza(dough){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            const pizza= dough+"pizza";
+            // console.log("here is the pizza", pizza);
+            resolve(pizza);
+        }, 2000);
+    })
+}
+
+
+getCheese()
+.then((cheese)=>{
+    console.log("here is the cheese",cheese);
+    return makeDough(cheese);
+
+})
+.then((dough)=>{
+    console.log("here is the dough",dough);
+    return bakePizza(dough);
+    
+})
+.then((pizza)=>{
+    console.log("here is the pizza",pizza);
+})
+.catch((data)=>{
+    console.log("error occuered",data);
+})
