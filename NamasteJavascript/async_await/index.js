d3153e63ad64a2ab350416b870a802ff47580142
Suraj-jha7 <123754719+Suraj-jha7@users.noreply.github.com>
2023-11-26 14:15:53 +0000

    more on async await

--- NamasteJavascript/async_await/index.js
+++ NamasteJavascript/async_await/index.js
@@ -3,6 +3,7 @@
 2. async and await combo are used to handle promises..
 3. await is a keyword which can only be used inside an async function..
 4. js engine looks like it wait for promise to be resolved in async await case but it does not , it appears to be waiting.
+but in actual it does not wait it suspend the function and run after the promise complete at previous point from it suspended.
 
 */
 
@@ -31,12 +32,31 @@ const p= new Promise((resolve,reject)=>{
 })
 
 
+const p2= new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("This is p2 promise");
+    }, 6000);
+})
+
+
+const p3 =new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("This is p3 promise");
+    }, 8000);
+})
 //Below we are using asyc await , in this js engine will wait for completion of promise after that below code will run..
 async function handlePromise(){
     console.log("Hello world........");
-const val=await p;
-console.log("orange javascript...")
-console.log(val);
+    const val=await p;
+    console.log("orange javascript...")
+    console.log(val);
+    const val2= await p2;
+    console.log(val2);
+
+    const val3= await p3;
+    console.log(val3);
+
+
 }
 
 console.log("testing .......1");
