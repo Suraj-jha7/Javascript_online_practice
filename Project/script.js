40e0991d9d9260fc01134126afd52ab4a0fb0748
Suraj-jha7 <123754719+Suraj-jha7@users.noreply.github.com>
2023-11-28 18:53:37 +0000

    comitted

--- Project/script.js
+++ Project/script.js
@@ -4,11 +4,13 @@ const input=document.querySelector(".form-control");
 
 console.log("hello");
 form1.addEventListener("submit",function (e){
-   
+    cardContainer.innerHTML=``
     e.preventDefault();
     fatchData(e.target[0].value);
     console.log("success....",e.target[0].value);
     input.value="";
+
+
 
 })
 
@@ -18,50 +20,28 @@ async function fatchData(data)
     const response = await fetch(`https://dummyjson.com/products/search?q=${data}`);
     const result= await response.json();
     console.log(result);
-    showData(result.products[1]);
+    productFetch(result.products);
 }
 
-
-function showData(result)
+function productFetch(result)
 {
-    console.log("this is result",result,);
-    const resultBody=`
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-          <div class="col">
-            <div class="card h-100">
-              <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src=${result.images[0]} class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src=${result.images[1]} class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src=${result.images[2]} class="d-block w-100" alt="...">
-                  </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-body-secondary">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+    console.log("this is result",result);
+    result.forEach(element => {
+        cardRender(element);
+    });
+}
 
-          
-        `
+function cardRender(result)
+{
+    console.log("this is 2nd result",result,);
+    const resultBody=`<div class="card" style="width: 18rem;">
+    <img src="${result.images[0]}" class="card-img-top" alt="..." min-height=220px min-widht=300px>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`
         cardContainer.insertAdjacentHTML("beforeend",resultBody);
 
 
