3d0df01cadd87e67a4e4fbe6064b45948afa2edb
Suraj-jha7 <123754719+Suraj-jha7@users.noreply.github.com>
2023-12-07 16:39:21 +0000

    new method added

--- projects/01Bgchanger/script.js
+++ projects/01Bgchanger/script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const red=document.getElementById("red");
+const body=document.getElementsByTagName("body");
+const green=document.getElementById("green");
+const blue=document.getElementById("blue");
+const btndiv=document.getElementById("btndiv");
+
+
+
+
+
+function changebg(e){
+console.log(e.target.innerText,"this is ");
+    // document.body.removeAttribute('class');
+    // document.body.classList.add(e.target.innerText);
+    document.body.style.backgroundColor = e.target.innerText;
+}
+
+// red.addEventListener("click",changebg)
+// green.addEventListener("click",changebg)
+// blue.addEventListener("click",changebg)
+btndiv.addEventListener("click",changebg)
+
+
